e89846a98a606ebfdbc5e2b62c600c22a43c66b4
CarloGabriel <carlosbiel23@gmail.com>
2023-04-30 13:20:02 -0400

    Hexagon tiles implementation
     Hexagon tiles implementation to tiles have different shapes
    Script modification to adequate to new tiles shape

--- UnityCarlosChallenge/Assets/Scripts/HexagonCharacterMovimentSquare.cs
+++ UnityCarlosChallenge/Assets/Scripts/HexagonCharacterMovimentSquare.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMoviment : MonoBehaviour
+public class HexagonCharacterMoviment : MonoBehaviour
 {
   
 
@@ -11,7 +11,7 @@ public class CharacterMoviment : MonoBehaviour
     void OnMouseDown()
     {
         GameObject PlayerToMove = GameObject.Find ("DogPolyart"); 
-        PlayerToMove.transform.position = new Vector3(transform.position.x,0.3f,transform.position.z);
+        PlayerToMove.transform.position = new Vector3(transform.position.x,0.2f,transform.position.z);
         PlayerToMove.transform.rotation = Quaternion.identity;
     }
 }
---
+++ UnityCarlosChallenge/Assets/Scripts/SquareCharacterMoviment.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareCharacterMoviment : MonoBehaviour
+{
+  
+
+   
+
+    void OnMouseDown()
+    {
+        GameObject PlayerToMove = GameObject.Find ("DogPolyart"); 
+        PlayerToMove.transform.position = new Vector3(transform.position.x,0.3f,transform.position.z);
+        PlayerToMove.transform.rotation = Quaternion.identity;
+    }
+}
